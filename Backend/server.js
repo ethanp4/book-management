155d2f78ca92f05bf2ce624fc35c278df86a15e7
eilidh <153418807+sneli@users.noreply.github.com>
2024-11-14 00:58:51 -0700

    Update server.js
    Updated the issues that Aurora has fixed

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,13 @@ const booksRouter = require('./routes/books');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+})
+
 app.post('/login', (req, res) => {
 
     const { username, password } = req.body;
