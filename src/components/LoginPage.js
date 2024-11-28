8d132df511c5868552be19b92ecf7be047362e23
“2023317MaxDyson <maxgdyson@gmail.com>
2024-11-28 12:24:23 -0700

    Addded css to the login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,3 +1,22 @@
+
+import './LoginPage.css';
 export default function LoginPage() {
-  
+    return (<div>
+        <h3> Bow Valley Library System Admin Login </h3>
+        <br />
+        <br />
+        <br />
+        <form>
+            <label> UserName: </label>
+            <input type="text" name="username" />
+            <br />
+            <br />
+            <label> Password: </label>
+            <input type="password" name="password" />
+            <br />
+            <br />
+            <button> Login </button>
+        </form>
+    </div>);
 }
+
